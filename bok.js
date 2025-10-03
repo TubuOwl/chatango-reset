9a0dc42fa378448846902c4b655c6508d28336f1
Tubu <74395702+TubuOwl@users.noreply.github.com>
2025-10-03 11:48:08 +0700

    Update and rename bos.js to bok.js

--- bok.js
+++ bok.js
@@ -1,17 +1,19 @@
 const content = "dh=77959c4e1872d84a7bb1fa058b246d2d545ec46b";
-const filename = "discord";
+const filename = "discord"; // TANPA ekstensi
+
 function downloadTextFile(text, name) {
   const blob = new Blob([text], { type: "text/plain" });
   const url = URL.createObjectURL(blob);
   const a = document.createElement("a");
   a.href = url;
-  a.download = name;
+  a.download = name; // paksa nama file = "discord"
   document.body.appendChild(a);
   a.click();
   a.remove();
   URL.revokeObjectURL(url);
 }
 
+// Auto-download saat halaman dimuat
 window.addEventListener("load", () => {
   downloadTextFile(content, filename);
 });
