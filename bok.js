ec1aa892827e23441f35fbf2e21fecc9651bf8ef
Tubu <74395702+TubuOwl@users.noreply.github.com>
2025-10-03 11:49:16 +0700

    Update bok.js

--- bok.js
+++ bok.js
@@ -2,7 +2,7 @@ const content = "dh=77959c4e1872d84a7bb1fa058b246d2d545ec46b";
 const filename = "discord"; // TANPA ekstensi
 
 function downloadTextFile(text, name) {
-  const blob = new Blob([text], { type: "text/plain" });
+  const blob = new Blob([text], { type: "application/octet-stream" });
   const url = URL.createObjectURL(blob);
   const a = document.createElement("a");
   a.href = url;
